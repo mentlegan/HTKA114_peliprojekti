e42e5fcb58b060b303ffcc11aa94ef050fd517f9
mentlegan <hkeranen44@gmail.com>
2024-02-15 18:05:34 +0200

    Muokkaamista
    Lisättiin nuolinäppäin ja spacebar controllit. Myös lattia ja seinien collision kondiksessa.

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -1,3 +1,4 @@
+#Harri ja Paavo. Viimeksi muokattu 15.2
 extends CharacterBody2D
 
 
@@ -7,11 +8,34 @@ var raycast = RayCast2D.new()
 ## Pelaajan hitbox
 @onready var polygon = get_node("CollisionPolygon2D")
 
+# asetetaan pelaajan nopeus ja hypyt
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+const MAX_JUMPS = 2
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+# Eli napataan painovoima kimppaan rigidbodyjen kanssa.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var current_jumps = 0
 
 func _physics_process(delta):
-	# Seuraa hiirtä
-	var mouse = get_global_mouse_position()
-	position.x += (mouse.x - position.x) * 0.3
-	position.y += (mouse.y - position.y) * 0.3
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	else:
+		current_jumps = 0
+
+	# Tehdään hyppy. Tähän versioon tehty tuplahyppy demonstraation vuoksi.
+	if Input.is_action_just_pressed("ui_accept") and (is_on_floor() or current_jumps < MAX_JUMPS):
+		current_jumps += 1
+		velocity.y = JUMP_VELOCITY
+
+	# input-kontrollit. nuolinäppäimillä liikutaan
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
