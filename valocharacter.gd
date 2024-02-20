6e141044bdc24febc7dc8a3c6d5a0c606f770cb7
Juuso Piippo <158200846+JuJuz1@users.noreply.github.com>
2024-02-20 16:43:03 +0200

    Valon ampuminen, pysayttaminen ja kimmoittaminen
    Valosta muutettu characterbody2d, jotta sille voi tehdä fysiikkaa. Valon voi ampua hiiren suuntaan vasemmasta painikkeesta ja pysäyttää oikeasta. Lisätty valon kimpoaminen. Poistettu alkuperäinen valo, sen voi lisätä takaisin suoraan filesystemistä halutessa.

--- valocharacter.gd
+++ valocharacter.gd
@@ -1,4 +1,4 @@
-extends StaticBody2D
+extends CharacterBody2D
 
 
 @onready var player = get_node("../Pelaaja")
@@ -8,24 +8,56 @@ extends StaticBody2D
 
 var raycast = RayCast2D.new()
 
+# Ei tietoa miten hyödyntää valon liikuttamisessa
+var SPEED = 10.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	raycast.collide_with_areas = true
-	raycast.collide_with_bodies = false
-	add_child(raycast)
+# Valon liikkumisen testaukseen
+var MOVING = false
+# Voi luultavasti jättää pois ja käyttää CharacterBody2D omaa velocity attribuuttia
+# alussa pysähdyksissä
+var move_vec = Vector2(0, 0)
 
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	# Hiirellä liikuttaminen
+func _physics_process(delta):
+	# Onko edes tarpeelliset, toimii ilman näitäkin
+	# raycast.collide_with_areas = true
+	# raycast.collide_with_bodies = false
+	# add_child(raycast)
+	
+	"""hiirellä liikuttaminen
 	var mouse = get_global_mouse_position()
 	self.position.x += (mouse.x - self.position.x) * 0.3
 	self.position.y += (mouse.y - self.position.y) * 0.3
-
-
-func _physics_process(delta):
-	raycast.target_position = Vector2(player.position.x - position.x, player.position.y - position.y)
+	"""
+	
+	# Valon suunnan antaminen hiiren vasemmalla painikkeella
+	# käytännössä samalla laitetaan liikkeelle
+	# enum MouseButton lukee MOUSE_BUTTON_LEFT = 1 eli voisi olla vain numero 1
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		# Hiiren suunta
+		var mouse = get_global_mouse_position()
+		# Suuntavektorin muodostus, antaa automaattisesti nopeuden etäisyyden perusteella
+		move_vec = Vector2(mouse.x - self.position.x, mouse.y - self.position.y)
+		MOVING = true
+	
+	#                                  tai pelkkä 2
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		# Alustetaan suuntavektori eli pysäytetään
+		move_vec = Vector2(0, 0)
+		MOVING = false
+	
+	# Liikkeessä
+	if MOVING:
+		# Valon liikkuminen
+		var collision = move_and_collide(move_vec * delta)
+		# Jos osuu johonkin
+		if collision:
+			# Kimpoaminen
+			move_vec = move_vec.bounce(collision.get_normal())
+	
+	raycast.target_position = Vector2(player.position.x - self.position.x, player.position.y - self.position.y)
 	
 	var space_state = get_world_2d().direct_space_state
 	
