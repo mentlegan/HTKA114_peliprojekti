bcc64182ace6e996cbd3ecb9ef10d5a8688db72c
patoporh <paavo.porho@iki.fi>
2024-02-28 13:10:29 +0200

    SmartShape2D:n törmäysten luonti varjojen luonnin yhteyteen

--- ss2d_varjot.gd
+++ ss2d_varjot.gd
@@ -1,33 +1,72 @@
 extends Node2D
 
 
-# TODO: Rivi 26: Tarkista, että CollisionPolygon2D on olemassa.
-# Ei pitäisi koskaan tapahtua, ellei sitä ole erikseen poistettu.
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# Lisätään StaticBody2D-nodeille varjot.
-	self.lisaa_varjot()
+	# Lisätään SS2D_Shape-nodeille varjot ja collisionit.
+	self.lisaa_varjot_ja_collisionit()
 
 
-## Lisää jokaiselle StaticBody2D-tyyppiselle lapsi-nodelle LightOccluder2D:n.
-## Toimiakseen, StaticBody2D-nodella on oltava lapsena CollisionPolygon2D.
-func lisaa_varjot():
+## Luo jokaiselle SS2D_Shape-lapsinodelle LightOccluder2D- ja CollisionPolygon2D-nodet.
+## Asettaa samalla SS2D_Shape-noden uudeksi vanhemmaksi StaticBody2D-noden, jotta
+## CollisionPolygon2D toimisi.
+##
+## Esimerkki nodejen rakenteesta ennen funktion kutsua:
+##
+##   Tiilet
+##   ├── SS2D_Shape
+##   └── SS2D_Shape2
+##
+## Funktiokutsun jälkeen:
+##
+##   Tiilet
+##   ├── StaticBody2D
+##   │   ├── CollisionPolygon2D
+##   │   ├── LightOccluder2D
+##   │   └── SS2D_Shape
+##   └── StaticBody2D2
+##       ├── CollisionPolygon2D
+##       ├── LightOccluder2D
+##       └── SS2D_Shape2
+##
+func lisaa_varjot_ja_collisionit():
 	# Käydään läpi jokainen lapsi-node.
-	for child in get_children():
-		# Lisätään varjot pelkästään StaticBody2D-nodeille.
-		if child is StaticBody2D:
-			# Luodaan LightOccluder2D ja sille tarvittava OccluderPolygon2D-node
+	for lapsi in get_children():
+		# Käsitellään pelkästään SS2D_Shape-nodeja
+		if lapsi is SS2D_Shape:
+			# Luodaan StaticBody2D, jonka lapseksi asetetaan nykyinen SS2D_Shape
+			var static_body = StaticBody2D.new()
+			lapsi.reparent(static_body)
+			
+			# Haetaan seuraavaksi SS2D_Shape:n pisteet ja luodaan niiden perusteella
+			# uusi PackedVector2Array, jonka pisteet on sovitettu oikein maailman koordinaatistoon
+			# SS2D_Shapen keskipisteen perusteella.
+			var ss2d_sijainti = lapsi.global_position # SS2D_Shapen keskipiste
+			# Hyödynnetään SS2D_Shapen pisteiden luomiseen noden omaa metodia.
+			var suhteelliset_pisteet = lapsi.generate_collision_points()
+			# Luodaan PackedVector2Array sovitetuille pisteille
+			var sovitetut_pisteet = PackedVector2Array()
+			
+			# Käydään SS2D_Shapen suhteelliset pisteet läpi
+			for piste in suhteelliset_pisteet:
+				# Luodaan Vector2, jonka koordinaatteja siirretään SS2D_Shapen keskipisteen verran
+				var siirretty_piste = Vector2(piste.x + ss2d_sijainti.x, piste.y + ss2d_sijainti.y)
+				sovitetut_pisteet.append(siirretty_piste)
+			
+			# Nyt on käytössä sovitetut pisteet, jonka perusteella luodaan nodet varjoille ja
+			# collisioneille
 			var occluder = LightOccluder2D.new()
 			var occluder_polygon = OccluderPolygon2D.new()
+			var collision_polygon = CollisionPolygon2D.new()
 			
-			# Haetaan varjolle asetettava polygoni CollisionPolygon2D:ltä ja
-			# asetetaan se varjon polygoniksi.
-			var collision_polygon_2d = child.get_node("CollisionPolygon2D")
-			occluder_polygon.set_polygon(collision_polygon_2d.get_polygon())
+			# Asetetaan sovitetut pisteet nodejen polygoneiksi
+			occluder_polygon.set_polygon(sovitetut_pisteet)
 			occluder.set_occluder_polygon(occluder_polygon)
+			collision_polygon.set_polygon(sovitetut_pisteet)
 			
-			# Lisätään lopuksi LightOccluder2D nykyisen StaticBody2D-noden
-			# lapseksi.
-			child.add_child(occluder)
+			# Lisätään varjot ja collisionit StaticBody2D:n lapsiksi
+			static_body.add_child(occluder)
+			static_body.add_child(collision_polygon)
+			
+			# Lisätään lopuksi StaticBody2D Tiilet-noden lapseksi
+			self.add_child(static_body)
