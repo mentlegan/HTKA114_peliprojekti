36a7f9e311e69bc7ec0c50c6db3ed7b9187502de
Juuso Piippo <158200846+JuJuz1@users.noreply.github.com>
2024-03-07 15:31:03 +0200

    Valon liikkuminen vakionopeudella

--- valocharacter.gd
+++ valocharacter.gd
@@ -1,6 +1,5 @@
-## Juuso 27.2.2024
+## Juuso 7.3.2024
 ## TODO: pelaajan hyppy- ja juoksuanimaatiot
-## TODO: kaikkien valonlähteiden etsiminen ja sitten raycast tarkistaminen
 extends CharacterBody2D
 
 
@@ -8,55 +7,63 @@ extends CharacterBody2D
 # @onready var hitbox: CollisionShape2D = get_node("../Pelaaja/CollisionShape2D")
 # mallina staattinen tyypitys
 # @onready var light: PointLight2D = get_node("PointLight2D")
+var Light = preload("res://valo_character.tscn")
 
-# Ei tietoa miten hyödyntää valon liikuttamisessa
-var SPEED = 10.0
+# Valon nopeus
+var SPEED = 130.0
 
-# Valon liikkumisen testaukseen
+# Valon liikkumisen ja luomisen testaukseen
 var MOVING = false
-
-# Voi luultavasti jättää pois ja käyttää CharacterBody2D omaa velocity attribuuttia
-# alussa pysähdyksissä
-var move_vec = Vector2(0, 0)
+var EXISTS = true
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+
 func _physics_process(delta):
+	print(EXISTS)
 	# Onko edes tarpeelliset, toimii ilman näitäkin
 	# raycast.collide_with_areas = true
 	# raycast.collide_with_bodies = false
 	# add_child(raycast)
-	
-	"""hiirellä liikuttaminen
-	var mouse = get_global_mouse_position()
-	self.position.x += (mouse.x - self.position.x) * 0.3
-	self.position.y += (mouse.y - self.position.y) * 0.3
-	"""
 	
 	# Valon suunnan antaminen hiiren vasemmalla painikkeella
 	# käytännössä samalla laitetaan liikkeelle
 	if Input.is_action_just_pressed("painike_vasen"):
 		# Hiiren suunta
 		var mouse = get_global_mouse_position()
-		# Suuntavektorin muodostus, antaa automaattisesti nopeuden etäisyyden perusteella
-		move_vec = Vector2(mouse.x - self.position.x, mouse.y - self.position.y)
+		
+		if not EXISTS:
+			# Valon synnyttäminen
+			var l = Light.instantiate()
+			# Lisääminen puuhun
+			l.start(player.position)
+			get_tree().root.add_child(l)
+			# self.position = player.position
+			EXISTS = true
+		
+		# Normalisoidun suunnan laskeminen valopallon sijainnista klikattuun kohtaan
+		var direction = Vector2(self.position.x, self.position.y).direction_to(mouse)
+		velocity = direction * SPEED
 		MOVING = true
 	
 	if Input.is_action_just_pressed("painike_oikea"):
 		if not MOVING:
-			self.position.x = player.position.x
-			self.position.y = player.position.y
+			# self.position.x = player.position.x
+			# self.position.y = player.position.y
+			# Poistetaan valo
+			EXISTS = false
+			queue_free()
 		else: 
 			# Alustetaan suuntavektori eli pysäytetään
-			move_vec = Vector2(0, 0)
+			velocity = Vector2(0, 0)
 			MOVING = false
 	
 	# Liikkeessä
 	if MOVING:
 		# Valon liikkuminen
-		var collision = move_and_collide(move_vec * delta)
+		var collision = move_and_collide(velocity * delta)
 		# Jos osuu johonkin
 		if collision:
 			# Kimpoaminen
-			move_vec = move_vec.bounce(collision.get_normal())
+			velocity = velocity.bounce(collision.get_normal())
