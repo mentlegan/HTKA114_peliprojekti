eb13168ab8e020ef885962bcbe71a594b0f8541e
patoporh <paavo.porho@iki.fi>
2024-03-15 11:32:20 +0200

    Alustava buginen toteutus vihollisen pakenemiselle
    Vihollinen etsii sisarus-nodeista kuopat, jonne pyrkii vuorotellen kaivautumaan. Pakeneminen alkaa, kun vihollinen osuu valoon ja pysyy siinä tilassa, kunnes pääsee kuopan luo.
    Tarvitsee vielä ajastimen, joka muuttaa vihollisen seuraavaa kuoppaa, jos pakenemistilassa ollaan liian kauan paikallaan.

--- vihollinen.gd
+++ vihollinen.gd
@@ -15,14 +15,26 @@ const IDLE_AUDIO_AJASTIN_MAX = 15.0 ## Ajastin asetetaan satunnaisesti 50-100%:i
 @onready var audio_jahtaus = $AudioJahtaus
 @onready var audio_pakeneminen = $AudioPakeneminen
 
+## Valontarkistus-node
+@onready var valon_tarkistus = $ValonTarkistus
+
+## Keho-node
+@onready var keho = $Keho
+
 var jahdissa = false ## Vakiona vihollinen ei ole jahtaamassa pelaajaa
 var pakenee = false
 var pelaaja = null
+var valossa = false
+var menossa_kuoppaan = null
 
-var kuopat = Array() ## Taulukko kuopille, joihin vihollinen voi kaivautua
+## Taulukko kuopille, joihin vihollinen voi kaivautua
+var kuopat = Array()
+## Kuoppa, jonne vihollinen on viimeksi kaivautunut
+var kuoppa_indeksi = 0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 
 func _ready():
 	# Ajastimen toimintaa varten kutsutaan randomize()
@@ -41,6 +53,42 @@ func _ready():
 	for sisarus in get_parent().get_children():
 		if sisarus is Kuoppa:
 			kuopat.append(sisarus)
+	
+	# Yhdistetään valon signaalit vihollisen omiin funktioihin
+	valon_tarkistus.connect("siirrytty_valoon", siirrytty_valoon)
+	valon_tarkistus.connect("siirrytty_varjoon", siirrytty_varjoon)
+
+
+## Etsii vihollisen kuopista seuraavan, jonne paeta
+func seuraava_kuoppa():
+	menossa_kuoppaan = kuopat[kuoppa_indeksi]
+	kuoppa_indeksi = (kuoppa_indeksi + 1) % len(kuopat)
+
+
+## Kun siirrytään valoon
+## Tarkistetaan samalla, onko vihollinen kuopan päällä, jonne lähtisi menemään
+func siirrytty_valoon():
+	# Jatketaan pakenemista, jos vihollinen ei ole kerennyt vielä kuoppaan
+	if pakenee:
+		return
+	
+	pakenee = true
+	jahdissa = false
+	valossa = true
+	seuraava_kuoppa()
+	
+	for node in keho.get_overlapping_areas():
+		if node == menossa_kuoppaan:
+			lopeta_pakeneminen()
+
+
+## Kun siirrytään varjoon
+func siirrytty_varjoon():
+	# Jatketaan pakenemista, jos vihollinen ei ole kerennyt vielä kuoppaan
+	if pakenee:
+		return
+	
+	valossa = false
 
 
 ## Aloittaa ajastimen idle-ääniefektille
@@ -48,11 +96,22 @@ func aloita_idle_audio_ajastin():
 	idle_audio_ajastin.start((1 - randf() * 0.5) * IDLE_AUDIO_AJASTIN_MAX)
 
 
+## Lopettaa pakenemisen ja teleportaa seuraavaan kuoppaan
+func lopeta_pakeneminen():
+	pakenee = false
+	valossa = false
+	seuraava_kuoppa()
+	print(menossa_kuoppaan)
+	self.global_position = menossa_kuoppaan.global_position
+
+
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta # Painovoimaa vihollisellekin
 	if jahdissa: # Tässä "jahtimoodi" eli kun vihollinen tietää, että missä pelaaja on
 		position += (pelaaja.position - position)/SPEED
+	elif pakenee:
+		position += (menossa_kuoppaan.global_position - position)/SPEED
 	move_and_slide()
 
 
@@ -78,6 +137,10 @@ func _on_keho_body_entered(body):
 
 ## Jos pelaaja astuu vihollisen tietoisuusalueelle
 func _on_tarkkaavaisuus_body_entered(body):
+	# Jatketaan pakenemista, jos vihollinen ei ole kerennyt vielä kuoppaan
+	if pakenee:
+		return
+	
 	pelaaja = body
 	jahdissa = true
 	
@@ -93,9 +156,19 @@ func _on_tarkkaavaisuus_body_entered(body):
 
 ## Jos pelaaja poistuu vihollisen tietoisuusalueelta
 func _on_tarkkaavaisuus_body_exited(_body):
+	# Jatketaan pakenemista, jos vihollinen ei ole kerennyt vielä kuoppaan
+	if pakenee:
+		return
+	
 	pelaaja = null
 	jahdissa = false
 	
 	# Aloitetaan ajastin idle-ääniefektille, jos se on pois päältä
 	if idle_audio_ajastin.is_stopped():
 		aloita_idle_audio_ajastin()
+
+
+## Jos viholliseen osuu paetessa kuoppaan, lopetetaan pakeneminen ja teleportataan seuraavaan
+func _on_keho_area_entered(area):
+	if area is Kuoppa and pakenee:
+		lopeta_pakeneminen()
