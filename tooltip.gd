adb1a7d1cf1d59e3d74b56fa625d2053bf99a15e
patoporh <paavo.porho@iki.fi>
2024-03-17 19:16:07 +0200

    Lisää tooltippejä

--- tooltip.gd
+++ tooltip.gd
@@ -1,30 +1,18 @@
-extends Sprite2D
+extends Area2D
 
 
-const OFFSET_MAX = 15
-const OFFSET_SPEED = 15
-var offset_target = 0
-
-
+## Piilotetaan tooltip pelin alussa
 func _ready():
-	offset_target = OFFSET_MAX
-	offset.y = OFFSET_MAX
-	modulate.a = 0
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	offset.y += (offset_target - offset.y) * delta * OFFSET_SPEED
-	modulate.a = 1 - (offset.y / OFFSET_MAX)
+	visible = false
 
 
 ## Asetetaan tooltip näkyviin, kun pelaaja astuu sen alueelle
-func _on_area_2d_body_entered(body):
+func _on_body_entered(body):
 	if body is Pelaaja:
-		offset_target = 0
+		visible = true
 
 
 ## Piilotetaan tooltip, kun pelaaja poistuu sen alueelta
-func _on_area_2d_body_exited(body):
+func _on_body_exited(body):
 	if body is Pelaaja:
-		offset_target = OFFSET_MAX
+		visible = false
