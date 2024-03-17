5d86a137265365f9f5d30adad3923cff3b01c84b
patoporh <paavo.porho@iki.fi>
2024-03-17 14:04:28 +0200

    Game over näyttöön ohjaimen tuki

--- gameover_ruutu.gd
+++ gameover_ruutu.gd
@@ -9,13 +9,27 @@ var kuolemateksti = [
 	"Darkness has fallen"
 ]
 
+
 ## Readyssa otetaan satunnainen teksti taulukosta, ja asetetaan se ruutuun
 func _ready():
 	$Paneeli/MuuttuvaTeksti.text = kuolemateksti[randi() % kuolemateksti.size()]
 
+
+func _process(_delta):
+	if not visible:
+		return
+	
+	# "Painetaan" nappeja ohjaimella
+	if Input.is_action_just_pressed("abxy_oikea"):
+		_on_lopeta_nappi_pressed()
+	if Input.is_action_just_pressed("abxy_alas"):
+		_on_restart_nappi_pressed()
+
+
 ## Kun painaistaan quit-nappulaa
 func _on_lopeta_nappi_pressed():
 	get_tree().quit() #Napataan tree ja peli loppuu quitilla
+
 
 ## Kun painaistaan restart-nappulaa
 func _on_restart_nappi_pressed():
