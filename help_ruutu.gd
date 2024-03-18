efef84b8012bc6d5c7bbe50856ed79996810c1f8
patoporh <paavo.porho@iki.fi>
2024-03-18 10:34:16 +0200

    Help-ruudun korjaus

--- help_ruutu.gd
+++ help_ruutu.gd
@@ -2,7 +2,6 @@ extends Control
 
 
 func _process(_delta):
-	if (Input.is_action_just_pressed("help")
-	or Input.is_action_just_pressed("abxy_oikea")):
+	if Input.is_action_just_pressed("help"):
 		self.visible = not self.visible
 		get_tree().paused = self.visible
