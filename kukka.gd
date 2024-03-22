eecdfde3dcf3163d3132f1875c9a51b722af8cc5
Juuso Piippo <158200846+JuJuz1@users.noreply.github.com>
2024-03-22 14:25:45 +0200

    Kukille valot, valoja saadetty

--- kukka.gd
+++ kukka.gd
@@ -1,7 +1,10 @@
-## Juuso 13.3.2024
+## Juuso 22.3.2024
 ## Kukkien ominaisuudet luonnossa
 
 extends Area2D
+
+## Ladataan valo valmiiksi
+var kukka_valo = preload("res://kukka_valo.tscn")
 
 ## Tällä hetkellä toteutettu pelaaja-skriptissä kukkien area2D nodejen sekä ryhmien avulla
 """
@@ -11,6 +14,13 @@ func _on_body_entered(body):
 		Globaali.palloja = 2 # Tästä globaaliin muuttujaan muutos, että palloja on kaksi käytettävänä
 		print("kukka kerätty")
 """
+
+## Valon lisääminen pallon osuttua
+func _on_body_entered(body):
+	if body.is_in_group("valopallo") and self.get_child_count() < 3:
+		var valo = kukka_valo.instantiate()
+		self.add_child(valo)
+		self.add_to_group("valonlahde")
 		
 		# queue_free() # Poistetaan kukka luonnosta, kun se on kerätty
 		# Ei poisteta ainakaan ensimmäisiä kukkia, jotta valopalloja voi ottaa loputtomasti
