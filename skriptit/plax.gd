741f99bf6e73bdd0dd04f6bf2ffbb741ef73cb81
patrikhagg <haggpatrik1@gmail.com>
2024-04-18 17:21:48 +0300

    Parallex taustojen nimien ja kommienttien parantelua

--- skriptit/plax.gd
+++ skriptit/plax.gd
@@ -4,6 +4,6 @@ extends Sprite2D
 var nopeusEro = 0.02
 @onready var pelaaja = $"../../../Pelaaja"
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Liikutetaan taustoja eri nopeuksilla pelaajaan verrattuna
 func _process(delta):
 	position.x = -pelaaja.position.x * taso * nopeusEro
