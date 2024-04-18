2aa4bece33eb0619a352a263b5f9e12af6874109
patrikhagg <haggpatrik1@gmail.com>
2024-04-18 16:57:35 +0300

    Parallex taustat dirteiksi ja nopeudet pienemmäksi

--- skriptit/plax.gd
+++ skriptit/plax.gd
@@ -1,7 +1,7 @@
 extends Sprite2D
 
 @export var taso = 1
-var nopeusEro = 0.1
+var nopeusEro = 0.02
 @onready var pelaaja = $"../../../Pelaaja"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
