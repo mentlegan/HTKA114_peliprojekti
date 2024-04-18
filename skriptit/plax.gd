95f977f8622dc2e7b12956ffc8b093b1fbf1d042
patrikhagg <haggpatrik1@gmail.com>
2024-04-18 16:48:35 +0300

    Parallex taustalle toiminta kuntoon
    Taustat liikkuu nyt eri tahdeissa

--- skriptit/plax.gd
+++ skriptit/plax.gd
@@ -1,9 +1,9 @@
-extends Polygon2D
+extends Sprite2D
 
 @export var taso = 1
-var nopeusEro = 0.2
-@onready var pelaaja = $"../../Pelaaja"
+var nopeusEro = 0.1
+@onready var pelaaja = $"../../../Pelaaja"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position = -pelaaja.get_global_position() * taso * nopeusEro
+	position.x = -pelaaja.position.x * taso * nopeusEro
