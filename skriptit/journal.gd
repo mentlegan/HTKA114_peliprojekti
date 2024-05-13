e6f0ef83c751008bec62607d8c5d8e0147294e60
patoporh <paavo.porho@iki.fi>
2024-05-13 10:16:36 +0300

    Sivunumerot journaliin

--- skriptit/journal.gd
+++ skriptit/journal.gd
@@ -1,7 +1,36 @@
 extends Control
 
 
-@onready var sivut = $Sivut
+@onready var sivut_sprite = $Sivut
+@onready var sivunumero_label = $SivuLabel
+
+
+var sivut = {
+	"viimeisin_sivu": 0,
+	"nykyinen_sivu": 0
+}
+
+
+func _ready():
+	paivita_sivunumero()
+
+
+## Lisää journaliin tekstipätkän annettuun sivunumeroon.
+func lisaa_sivu(teksti: String, sivunumero: int):
+	# Ei lisätä duplikaatteja sivuja
+	if sivut.has(sivunumero):
+		return
+
+	sivut[sivunumero] = teksti
+	if sivunumero > sivut["viimeisin_sivu"]:
+		sivut["viimeisin_sivu"] = sivunumero
+	
+	paivita_sivunumero()
+
+
+## Päivittää sivunumero-labelin arvon.
+func paivita_sivunumero():
+	sivunumero_label.set_text(str(sivut["nykyinen_sivu"]) + "/" + str(sivut["viimeisin_sivu"]))
 
 
 ## Käsitellään input journalin ollessa aktiivinen
@@ -19,7 +48,6 @@ func _input(_event: InputEvent) -> void:
 ## Vaihtaa sivua eteen- tai taaksepäin annetun kokonaisluvun verran.
 ## Vaihtaa samalla sivun spriteä.
 func vaihda_sivua(delta):
-	sivut.texture.set_current_frame(
-		(sivut.texture.get_current_frame() + 1) % sivut.texture.get_frames()
+	sivut_sprite.texture.set_current_frame(
+		(sivut_sprite.texture.get_current_frame() + 1) % sivut_sprite.texture.get_frames()
 	)
-	print(delta)
