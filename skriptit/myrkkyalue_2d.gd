1f0905302081a009a512146a06b610fbdffed70a
patrikhagg <haggpatrik1@gmail.com>
2025-01-26 11:26:57 +0200

    Myrkkyalueelle uudet toiminnot
    Toimii nyt niin, että myrkkyalue_2d:lle asetetaan lapseksi yksi tai useampi polygon2d, ja niiden pohjalta luodaan halutut myrkkyalueet.

--- skriptit/myrkkyalue_2d.gd
+++ skriptit/myrkkyalue_2d.gd
@@ -2,8 +2,18 @@ extends Area2D
 ## Editorissa vaihdettava arvo sille, ottaako myrkystä damagea
 @export var tekeeko_damagea: bool = true
 
+## Animoitu texture myrkkyalueelle
+var myrkynTexture = preload("res://tres-tiedostot/myrkkyalue.tres")
+
 func _ready() -> void:
-	$AnimatedSprite2D.play("default")
+	for lapsi in get_children():
+		if lapsi is Polygon2D:
+			var myrkkyalue_collision = CollisionPolygon2D.new()
+			lapsi.texture = myrkynTexture
+			lapsi.modulate.a = 0.7
+			lapsi.texture_repeat = CanvasItem.TEXTURE_REPEAT_ENABLED
+			myrkkyalue_collision.polygon = lapsi.polygon
+			add_child(myrkkyalue_collision)
 
 
 ## Pelaajan alkaa ottamaan myrkkyä tullessaan alueelle
