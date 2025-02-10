1995938a1192ebadcf74037388191f97164257cf
Juuso Piippo <juuso.piippo1@gmail.com>
2025-02-10 16:38:59 +0200

    Refaktoroitu perhonen, checkpoint vesiputoukselle

--- skriptit/perhonen.gd
+++ skriptit/perhonen.gd
@@ -18,10 +18,9 @@ var edeltava_x = position.x
 ## Perhosen tämänhetkinen etäisyys reitillä. Liukuluku, 0..1 välissä
 var etaisyys = 0
 ## Perhosen aloituspiste, joka asetetaan _ready()-kutsun aikana
-var aloituspiste = null
+var aloituspiste = global_position
 ## Perhosen reitin pituus, min 1
 var reitin_pituus = 1
-
 
 ## Ready kutsutaan scenen avaamisessa
 func _ready():
@@ -33,30 +32,20 @@ func _ready():
 	aanen_ajastin.start()
 	
 	# Etsitään perhosen path2d lapsi-nodeista, jos sellainen on olemassa
-	var path2d_jo_lapsena = false
 	for lapsi in self.get_children():
 		if lapsi is Path2D:
-			path2d_jo_lapsena = true
 			path2d = lapsi
 			break
 	
-	# Jos Path2D-nodea ei ole asetettu editorissa, lisätään tyhjä Path2D lapseksi
-	if not path2d_jo_lapsena:
-		path2d = Path2D.new()
-		self.add_child(path2d)
-	
-	# Lisätään Path2D:n lapseksi PathFollow2D
-	path2d.add_child(path_follow_2d)
-	
-	# Asetetaan perhosen aloituspiste
-	aloituspiste = global_position + path2d.position
-
-	# Asetetaan perhosen reitin pituus
-	var curve2d = path2d.get_curve()
-	if curve2d:
-		reitin_pituus = max(1, path2d.get_curve().get_baked_length())
-	else:
-		reitin_pituus = 1
+	# Lisätään Path2D:n lapseksi PathFollow2D ja muokataan aloituspistettä
+	if path2d:
+		path2d.add_child(path_follow_2d)
+		aloituspiste = global_position + path2d.position
+		
+		# Asetetaan perhosen reitin pituus
+		var curve2d: Curve2D = path2d.get_curve()
+		if curve2d:
+			reitin_pituus = max(1, curve2d.get_baked_length())
 
 
 ## Kutsutaan, kun aanen_ajastin aika menee loppuun
@@ -67,6 +56,9 @@ func on_aanen_ajastin_timeout():
 
 ## Kutsutaan joka framella
 func _physics_process(delta: float) -> void:
+	if not path2d:
+		return
+	
 	# Kuljetetaan pathfollow2d-nodea reitillä eteenpäin
 	etaisyys += (delta * nopeus) / reitin_pituus
 	path_follow_2d.set_progress_ratio(etaisyys)
