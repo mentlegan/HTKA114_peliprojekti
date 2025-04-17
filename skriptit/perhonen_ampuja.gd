0f73ce42b1382218698ff424929d1987ead0cc5c
patoporh <paavo.porho@iki.fi>
2025-04-17 11:18:27 +0300

    Taistelija-perhonen seuraamaan pelaajaa
    Samalla korjattu (varmaan vain väliaikaisesti) UID-virheitä tallentamalla tscn- ja tres-tiedostoja uudestaan

---
+++ skriptit/perhonen_ampuja.gd
@@ -0,0 +1,45 @@
+extends Perhonen
+class_name PerhonenAmpuja
+## Alustava skripti ampuvalle perhoselle.
+
+
+## (Testausta varten)
+## Asettaa pelin alkaessa kohteen polunetsinnälle.
+@export var kohde: Marker2D
+
+## Ajastin, kuinka tiheästi perhonen päivittää kohteen pelaajaan
+@onready var kohteen_asetus_timer = $KohteenAsetusTimer
+## Seuraako perhonen pelaajaa vai ei
+var seuraa_pelaajaa = false
+
+var pelaaja = null
+
+
+func _physics_process(delta: float) -> void:
+	if seuraa_pelaajaa:
+		super._physics_process(delta)
+
+
+## Kun pelaaja poistuu perhosen etäisyydeltä
+func _on_body_exited(body: Node2D) -> void:
+	if body is Pelaaja:
+		seuraa_pelaajaa = false
+		pelaaja = body
+		kohteen_asetus_timer.stop()
+
+
+## Kun pelaaja tulee lähelle perhosta
+func _on_body_entered(body: Node2D) -> void:
+	if body is Pelaaja and polunetsija:
+		seuraa_pelaajaa = true
+		pelaaja = body
+		polunetsija.aseta_kohde(body.global_position)
+		kohteen_asetus_timer.start()
+
+
+## Asetetaan uusi kohde pelaajaan
+func _on_kohteen_asetus_timer_timeout() -> void:
+	if seuraa_pelaajaa and pelaaja:
+		polunetsija.aseta_kohde(pelaaja.global_position)
+	else:
+		kohteen_asetus_timer.stop()
--- skriptit/polunetsija.gd
+++ skriptit/polunetsija.gd
@@ -1,47 +1,44 @@
 ## Polunetsijä-node, joka liikuttaa vanhempaa NavigationRegion2D-nodejen sisällä.
 ## Kohde asetetaan aseta_kohde()-funktiolla, tai (testatessa) asettamalla
-## "Marker2D"-node Polunetsijan lapseksi.
+## Marker2D-node PerhonenCombat-noden Kohde-muuttujaan editorissa.
 extends NavigationAgent2D
 class_name Polunetsija
 
 
 ## Polunetsijän nopeus. Korvataan vanhemman nopeudella, jos "nopeus"-muuttuja
-## on olemassa vanhemmalla.
+## on olemassa.
 @onready var nopeus = 100.0
 var vanhempi = null
 
 
 func _ready():
-    vanhempi = get_parent()
+	vanhempi = get_parent()
 
-    if "nopeus" in vanhempi:
-        nopeus = vanhempi.nopeus
-    else:
-        print_debug("VAROITUS: Polunetsijän vanhemmalla %s ei ole 'nopeus'-muuttujaa, käytetään oletusarvoa." % vanhempi.get_path())
-    
-    for lapsi in self.get_children():
-        if lapsi is Marker2D:
-            aseta_kohde(lapsi.global_position)
-            break
-    
+	if "nopeus" in vanhempi:
+		nopeus = vanhempi.nopeus
+	else:
+		print_debug("VAROITUS: Polunetsijän vanhemmalla %s ei ole 'nopeus'-muuttujaa, käytetään oletusarvoa." % vanhempi.get_path())
+	
+	if vanhempi.kohde:
+		aseta_kohde(vanhempi.kohde.global_position)
 
 
 ## Asettaa polunetsijälle kohteen globaalina koordinaattina.
 func aseta_kohde(_global_position):
-    self.target_position = _global_position
-    print("Kohde asetettu: " + str(self.target_position))
+	self.target_position = _global_position
+	#print("Polunetsijän kohde asetettu: " + str(self.target_position))
 
 
 ## Liikkuu asetettua kohdetta päin, jos Polunetsijan vanhempi on
 ## NavigationRegion2D-noden sisällä.
 func liikuta_vanhempaa(delta):
-    if self.is_navigation_finished():
-        return
+	if self.is_navigation_finished():
+		return
 
-    # Haetaan seuraava sijainti
-    var seuraava_sijainti = self.get_next_path_position()
-    # Asetetaan Polunetsijan vauhti
-    velocity = vanhempi.global_position.direction_to(seuraava_sijainti) \
-                * nopeus * delta
-    # Liikutetaan vanhempaa
-    vanhempi.position += self.get_velocity()
+	# Haetaan seuraava sijainti
+	var seuraava_sijainti = self.get_next_path_position()
+	# Asetetaan Polunetsijan vauhti
+	velocity = vanhempi.global_position.direction_to(seuraava_sijainti) \
+				* nopeus * delta
+	# Liikutetaan vanhempaa
+	vanhempi.position += self.get_velocity()
